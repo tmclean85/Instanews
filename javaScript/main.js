a95ec8f39635b077c62e93fd5eb85d9ce6719d60
tmclean85 <trevor.a.mclean@gmail.com>
2017-04-29 22:30:05 -0700

    Added loading gif jS / CSS

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,52 +1,53 @@
 
-  $(function(){
+$(function () {
 
-var storiesList = $('#story-item')
+  var storiesList = $('#story-item')
+ 
+  $('.loading-gif').hide();
 
-$('select').change(function(){
-$(storiesList).empty();  
-  
-var url = 'https://api.nytimes.com/svc/topstories/v2/';
-  url+=$(this).val();
-  url+='.json';
-  url += '?' + $.param({'api-key': '2eb56f19cf854564af82b4d0641928ec'});
-  
-$.ajax({
+
+  $('select').change(function () {
+    $(storiesList).empty();
+    $('.loading-gif').show();
+
+
+    var url = 'https://api.nytimes.com/svc/topstories/v2/';
+    url += $(this).val();
+    url += '.json';
+    url += '?' + $.param({ 'api-key': '2eb56f19cf854564af82b4d0641928ec' });
+
+    $.ajax({
       url: url,
       method: 'GET',
-    }).done(function(data) {
-    $.each(data.results, function(index, value){
-}); 
- var articleGroup = data.results.filter(function (item) {
-      return item.multimedia.length; 
-    }).slice(0, 12);
+    }).done(function (data) {
+      $.each(data.results, function (index, value) {
+      });
+      var articleGroup = data.results.filter(function (item) {
+        return item.multimedia.length;
+      }).slice(0, 12);
 
-     $.each(articleGroup, function(key, value) {
-      var url = value.url;
-      var pic = value.multimedia[4].url;
-      var title = value.title;
-      var caption = value.abstract;
-      var appendItem = '';
+      $.each(articleGroup, function (key, value) {
+        var url = value.url;
+        var pic = value.multimedia[4].url;
+        var title = value.title;
+        var caption = value.abstract;
+        var appendItem = '';
 
+        console.log(title)
+        appendItem += '<div class="story-box-container" style="background-image: url('
+        appendItem += pic
+        appendItem += '");"><li class="story-box"><a href="';
+        appendItem += url;
+        appendItem += '"><div class="text-box"><p>';
+        appendItem += caption;
+        appendItem += '</p></div></a></li></div>';
+        $('#story-item').append(appendItem)
 
-    
-        
-  
-  console.log(title)
-      appendItem += '<div class="story-box-container" style="background-image: url('
-      appendItem += pic
-      appendItem += '");"><li class="story-box"><a href="';
-      appendItem += url;
-      appendItem += '"><p>';
-      appendItem += caption;
-      appendItem += '</p></a></li></div>';
-       $('#story-item').append(appendItem)
-      
-console.log(pic)
-       });
-     });
+        $('.loading-gif').hide();
+      });
+    });
   });
- });
+});
 
 
    
