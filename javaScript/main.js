f304b90a6cd2546309c5135634011728a17f3ea3
tmclean85 <trevor.a.mclean@gmail.com>
2017-05-01 19:06:34 -0700

    Implemented media queries and mixins

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,49 +1,51 @@
+$('.loading-gif').hide();
+$(document).ready(function () {
+  $(function () {
 
-$(function () {
-
-  var storiesList = $('#story-item')
- 
-  $('.loading-gif').hide();
-
-
-  $('select').change(function () {
-    $(storiesList).empty();
-    $('.loading-gif').show();
+    var storiesList = $('#story-item')
 
 
-    var url = 'https://api.nytimes.com/svc/topstories/v2/';
-    url += $(this).val();
-    url += '.json';
-    url += '?' + $.param({ 'api-key': '2eb56f19cf854564af82b4d0641928ec' });
 
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function (data) {
-      $.each(data.results, function (index, value) {
-      });
-      var articleGroup = data.results.filter(function (item) {
-        return item.multimedia.length;
-      }).slice(0, 12);
 
-      $.each(articleGroup, function (key, value) {
-        var url = value.url;
-        var pic = value.multimedia[4].url;
-        var title = value.title;
-        var caption = value.abstract;
-        var appendItem = '';
+    $('select').change(function () {
+      $(storiesList).empty();
+      $('.loading-gif').show();
 
-        console.log(title)
-        appendItem += '<div class="story-box-container" style="background-image: url('
-        appendItem += pic
-        appendItem += '");"><li class="story-box"><a href="';
-        appendItem += url;
-        appendItem += '"><div class="text-box"><p>';
-        appendItem += caption;
-        appendItem += '</p></div></a></li></div>';
-        $('#story-item').append(appendItem)
 
-        $('.loading-gif').hide();
+      var url = 'https://api.nytimes.com/svc/topstories/v2/';
+      url += $(this).val();
+      url += '.json';
+      url += '?' + $.param({ 'api-key': '2eb56f19cf854564af82b4d0641928ec' });
+
+      $.ajax({
+        url: url,
+        method: 'GET',
+      }).done(function (data) {
+        $.each(data.results, function (index, value) {
+        });
+        var articleGroup = data.results.filter(function (item) {
+          return item.multimedia.length;
+        }).slice(0, 12);
+
+        $.each(articleGroup, function (key, value) {
+          var url = value.url;
+          var pic = value.multimedia[4].url;
+          var title = value.title;
+          var caption = value.abstract;
+          var appendItem = '';
+
+          appendItem += '<div class="story-box-container"><li class="story-box" style="background-image: url('  
+          appendItem += pic
+          appendItem += '");"><a href="';
+          appendItem += url;
+          appendItem += '"><div class="text-box"><p>';
+          appendItem += caption;
+          appendItem += '</p></div></a></li></div>';
+        
+          $('#story-item').append(appendItem);
+          console.log(appendItem);
+          $('.loading-gif').hide();
+        });
       });
     });
   });
