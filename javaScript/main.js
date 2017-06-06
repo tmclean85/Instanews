78cf621a4e16a2844f29362c0fa1ecd9c26b806a
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-06 12:00:35 -0700

    Correct layout issues caused by previous commit

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,54 +1,54 @@
-$(document).ready(function(){
-
-$('.loading-gif').hide();
 $(document).ready(function () {
-  $(function () {
 
-    var storiesList = $('#story-item')
+  $('.loading-gif').hide();
+  $(document).ready(function () {
+    $(function () {
 
-    $('select').change(function () {
-      $(storiesList).empty();
-      $('.content-wrapper').removeClass('content-wrapper-alt')
-      $('.loading-gif').show();
-      $('.content-wrapper').addClass('content-wrapper-alt');
+      var storiesList = $('#story-item')
 
-      var url = 'https://api.nytimes.com/svc/topstories/v2/';
-      url += $(this).val();
-      url += '.json';
-      url += '?' + $.param({ 'api-key': '2eb56f19cf854564af82b4d0641928ec' });
+      $('select').change(function () {
+        $(storiesList).empty();
+        $('.content-wrapper').removeClass('content-wrapper-alt')
+        $('.loading-gif').show();
+        $('.content-wrapper').addClass('content-wrapper-alt');
 
-      $.ajax({
-        url: url,
-        method: 'GET',
-      }).done(function (data) {
-        $.each(data.results, function (index, value) {
-        });
-        var articleGroup = data.results.filter(function (item) {
-          return item.multimedia.length;
-        }).slice(0, 12);
+        var url = 'https://api.nytimes.com/svc/topstories/v2/';
+        url += $(this).val();
+        url += '.json';
+        url += '?' + $.param({ 'api-key': '2eb56f19cf854564af82b4d0641928ec' });
 
-        $.each(articleGroup, function (key, value) {
-          var url = value.url;
-          var pic = value.multimedia[4].url;
-          var title = value.title;
-          var caption = value.abstract;
-          var appendItem = '';
+        $.ajax({
+          url: url,
+          method: 'GET',
+        }).done(function (data) {
+          $.each(data.results, function (index, value) {
+          });
+          var articleGroup = data.results.filter(function (item) {
+            return item.multimedia.length;
+          }).slice(0, 12);
 
-          appendItem += '<a href="';
-          appendItem += url;
-          appendItem += '"><li class="story-box" style="background-image: url('
+          $.each(articleGroup, function (key, value) {
+            var url = value.url;
+            var pic = value.multimedia[4].url;
+            var title = value.title;
+            var caption = value.abstract;
+            var appendItem = '';
+
+          appendItem += '<div class="story-box-container"><li class="story-box" style="background-image: url('
           appendItem += pic
+          appendItem += '");"><a href="';
+          appendItem += url;
           appendItem += '"><div class="text-box"><p>';
           appendItem += caption;
-          appendItem += '</p></div>/li></a>';
+          appendItem += '</p></div></a></li></div>';
 
-          $('#story-item').append(appendItem);
-          $('.loading-gif').hide();
+            $('#story-item').append(appendItem);
+            $('.loading-gif').hide();
+          });
         });
       });
     });
   });
-});
 });
 
    
