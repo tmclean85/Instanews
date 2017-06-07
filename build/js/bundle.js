c28b474f23bf613ee5831985aa838c319a91f89f
tmclean85 <trevor.a.mclean@gmail.com>
2017-06-06 21:35:09 -0700

    Fixed indents/spacing

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -93,42 +93,42 @@ $(document).ready(function () {
           url: url,
           method: 'GET',
         })
-        .done(function (data) {
-          function validArticle(entry){
-            return entry.multimedia.length;
-          }
-          var articleGroup = data.results.filter(validArticle)
-                articleGroup.splice(12);
-          var appendItem = '';
-          $.each(articleGroup, function(item, value){
-            var url = value.url;
-            var pic = value.multimedia[4].url;
-            var caption = value.abstract;
-            appendItem += '<li class="story-box" style="background-image: url(';
-            appendItem += pic;
-            appendItem += ')"><a href="';
-            appendItem += url;
-            appendItem += '"><div class="text-box"><p>';
-            appendItem += caption;
-            appendItem += '</p></div></a></li>';
-          });     
+          .done(function (data) {
+            function validArticle(entry) {
+              return entry.multimedia.length;
+            }
+            var articleGroup = data.results.filter(validArticle)
+            articleGroup.splice(12);
+            var appendItem = '';
+            $.each(articleGroup, function (item, value) {
+              var url = value.url;
+              var pic = value.multimedia[4].url;
+              var caption = value.abstract;
+              appendItem += '<li class="story-box" style="background-image: url(';
+              appendItem += pic;
+              appendItem += ')"><a href="';
+              appendItem += url;
+              appendItem += '"><div class="text-box"><p>';
+              appendItem += caption;
+              appendItem += '</p></div></a></li>';
+            });
 
-          $('.loading-gif').hide();
-          $('#story-item').append(appendItem);
-        })
+            $('.loading-gif').hide();
+            $('#story-item').append(appendItem);
+          })
 
-        .fail(function (){
-          $('.loading-gif').hide();
-          $('#story-item').append(
-            '<h1>No stories available...</h1>'
-          )
-        });
+          .fail(function () {
+            $('.loading-gif').hide();
+            $('#story-item').append(
+              '<h1>No stories available...</h1>'
+            )
+          });
       });
     });
   });
 });
 
-   
+
 
 /***/ })
 /******/ ]);
